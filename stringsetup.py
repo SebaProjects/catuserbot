23900180d814baf566b3ef3b7dea019ea29e9a92
SebaProjects <95536492+SebaProjects@users.noreply.github.com>
2021-12-05 13:30:41 +0100

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python3
-# (c) https://t.me/TelethonChat/37677
-# This Source Code Form is subject to the terms of the GNU
-# General Public License, v.3.0. If a copy of the GPL was not distributed with this
-# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
-
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print(
-    """Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details"""
+    """Per favore, vai su my.telegram.org
+Accedi utilizzando il tuo account di Telegram
+Clicca su 'API Development Tools'
+Crea una nuova applicazione inserendo i dati richiesti"""
 )
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+APP_ID = int(input("Inserisci qui l'APP ID: "))
+API_HASH = input("Inserisci qui l'API ASH: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
